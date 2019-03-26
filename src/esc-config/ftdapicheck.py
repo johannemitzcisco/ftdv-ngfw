af134bf0f595e8ef83e344d34d16339067971b13
Johan Nemitz <jnemitz@cisco.com>
2019-03-25 20:19:18 -0400

    Working demo with scaling
    Signed-off-by: Johan Nemitz <jnemitz@cisco.com>

--- src/esc-config/ftdapicheck.py
+++ src/esc-config/ftdapicheck.py
@@ -25,8 +25,12 @@ ip_addr = get_ip_addr()
 if ip_addr == None:
     print "IP Address property must be specified"
     sys.exit(int(3))
+success_once = False
+with open("/var/ftdiapi.counter", "r") as file:
+    if file.read() == 's':
+        success_once = True
 URL = "https://"+ip_addr+"/api/fdm/v2/fdm/token"
-# sending get request and saving the response as response object 
+# sending get request and saving the response as response object
 payload = {'grant_type': 'password','username': 'apitester','password': 'apitester'}
 headers = {'Content-Type' : 'application/json', 'Accept' : 'application/json'}
 try:
@@ -36,10 +40,20 @@ try:
     # We are only checking that the API service responds, expect "unauthorized(400)"
     if r.status_code == 400:
         sys.exit(int(0))
+        # Record we were successful once
+        with open("/var/ftdiapi.counter", "w") as file:
+            file.write('s')
     else:
+        if success_once:
+            # Device was once alive
+            sys.exit(int(4))
         sys.exit(int(1))
 except Exception as e:
     print traceback.format_exc()
+    if success_once:
+        # Device was once alive
+        sys.exit(int(4))
     sys.exit(int(2))
+
 
 
